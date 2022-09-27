94b4e34b4345ec3b8f3090eabb52fd98a2971bd8
Chris Centaure <c.centaure@webintelligence.fr>
2022-09-27 11:49:21 +0200

    initial commit

---
+++ TestClass.php
@@ -0,0 +1,30 @@
+<?php
+
+class Test 
+{
+    private $num_a;
+    private $num_b;
+
+    public function add($a,$b)  
+    {
+        return $a + $b;
+    }
+
+    public function sub($a,$b)  
+    {
+        return $a - $b;
+    }
+
+    public function mul($a,$b)  
+    {
+        return $a * $b;
+    }
+
+    public function div($a,$b)  
+    {
+        return $a / $b;
+    }
+}
+
+
+?>
---
+++ exo.php
@@ -0,0 +1,8 @@
+<?php
+    require('./TestClass.php');
+
+    $test = new Test();
+
+    $add = $test->add(1,2);
+    print $add;
+?>
